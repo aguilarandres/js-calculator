6875a3dabe6825f59bdc81242098a34b5db5714f
andres <aguilar_andres@protonmail.com>
2022-04-28 20:23:44 -0700

    added text elements for displaying current and previous operand

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
 const allClearButton = document.querySelector('[data-all-clear]')
+
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
