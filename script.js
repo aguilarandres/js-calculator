d7a73726ccf47ee4959b3781d3e7949f27faa7c8
andres <aguilar_andres@protonmail.com>
2022-04-28 21:25:47 -0700

    implement selectOperation method

--- script.js
+++ script.js
@@ -25,7 +25,9 @@ class Calculator {
   }
 
   selectOperation(operation) {
-
+    this.operation = operation
+    this.previousOperand = this.currentOperand
+    this.currentOperand = ''
   }
 
   compute() {
