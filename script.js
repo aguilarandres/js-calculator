4a4808fd2650e8eeb9fd82404434a8c17461c4c1
andres <aguilar_andres@protonmail.com>
2022-04-28 20:17:57 -0700

    created buttons for numbers, arithmetic operators, equals and clear

---
+++ script.js
@@ -0,0 +1,4 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const allClearButton = document.querySelector('[data-all-clear]')
